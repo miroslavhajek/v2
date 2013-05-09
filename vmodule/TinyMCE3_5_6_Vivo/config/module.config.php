7ecddb2f95714f3da78e0e1a6fdfb8ea87a34d7b
David Lukas <david.lukas@zfdaily.com>
2013-05-09 08:43:53 +0200

    VP-48 TinyMCE view helper moved from vivoportal to the TinyMCE vmodule

--- vmodule/TinyMCE3_5_6_Vivo/config/module.config.php
+++ vmodule/TinyMCE3_5_6_Vivo/config/module.config.php
@@ -4,5 +4,9 @@
  *
  */
 return array(
-
+    'view_helpers' => array(
+        'invokables' => array(
+            'tiny_mce'              => 'TinyMCE3_5_6_Vivo\View\Helper\TinyMce',
+        ),
+    ),
 );
